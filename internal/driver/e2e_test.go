451e16df8a3f6b5dc92bb804e167e0fb074a505c
Duco van Amstel <duco.vanamstel@gmail.com>
2024-12-18 00:28:27 +0000

    WIP

---
+++ internal/driver/e2e_test.go
@@ -0,0 +1,21 @@
+//go:build e2e
+
+package driver
+
+import "testing"
+
+func TestDownload(t *testing.T)  {
+	t.Parallel()
+}
+
+func TestEnv(t *testing.T) {
+	t.Parallel()
+}
+
+func TestInvoke(t *testing.T) {
+	t.Parallel()
+}
+
+func TestSync(t *testing.T) {
+	t.Parallel()
+}
